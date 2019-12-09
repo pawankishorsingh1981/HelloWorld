5c584440436e21038efa817109e9201bc2715779
pawankishorsingh1981 <55680224+pawankishorsingh1981@users.noreply.github.com>
2019-12-09 18:57:24 +0530

    original master
    OED: original master

--- Demo.java
+++ Demo.java
@@ -1,7 +1,6 @@
 public class Demo{
   public static void main(String[] args){
     System.out.println("Hello World");
-    System.out.println("Second commit");
-    System.out.println("New branch for multithreading");
+    System.out.println("Original master");
   }
 }
